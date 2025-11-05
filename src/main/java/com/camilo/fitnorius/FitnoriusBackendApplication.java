68d11e1aabd971e40628de12c47d473679e240d4
Camilo Ramirez <camilormo654@gmail.com>
2025-11-05 09:09:17 -0500

    actualizacion banner corregido

--- src/main/java/com/camilo/fitnorius/FitnoriusBackendApplication.java
+++ src/main/java/com/camilo/fitnorius/FitnoriusBackendApplication.java
@@ -2,10 +2,13 @@ package com.camilo.fitnorius;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class FitnoriusBackendApplication {
+@SpringBootApplication(scanBasePackages = "com.camilo.fitnorius")
+public class FitnoriusBackendApplication extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(FitnoriusBackendApplication.class, args);
+        System.out.println("🚀 Fitnorius Backend iniciado correctamente...");
     }
 }
